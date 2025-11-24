f5b68fb91ec687cce69bb0f52b7e3567f7df256b
Marija Zelic <145926170+masazelic@users.noreply.github.com>
2025-11-24 09:15:21 +0100

    Update install.bat for conda commands and echo

--- install.bat
+++ install.bat
@@ -1,16 +1,16 @@
-@REM create an environment from the spec-file.yml and activate it
-conda env create -f spec-file.yml
-conda activate YeaZ
+REM create an environment from the spec-file.yml and activate it
+call conda env create -f spec-file.yml
+call conda activate YeaZ
 
-@REM install the yeaz package in the environment
+REM install the yeaz package in the environment
 pip install -e .
 
-@REM Get Reservoir and install it in the environment (Git LFS NEEDED)
+REM Get Reservoir and install it in the environment (Git LFS NEEDED)
 git clone https://github.com/rahi-lab/LYN-track-and-trace.git
 cd LYN-track-and-trace
 pip install -e .
 cd ..
 
-
-@REM Echo a message to indicate the installation process is complete
-echo "Installation completed. You can activate the environment using 'conda activate YeaZ'."
+REM Echo a message to indicate the installation process is complete
+echo Installation completed. You can activate the environment using "conda activate YeaZ".
+pause
